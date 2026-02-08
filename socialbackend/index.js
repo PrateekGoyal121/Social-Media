d04869c2ab689253ba091b8bb4dcd5e98e23b749
somil893 <mkg.somil2809@gmail.com>
2026-02-08 14:59:56 +0530

    new

--- socialbackend/index.js
+++ socialbackend/index.js
@@ -1,33 +1,22 @@
-const express = require('express');
-
-require("dotenv").config();
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const app = express();
-const path = require("path");
 const PORT = process.env.PORT || 4000;
 
-//cookie-parser - what is this and why we need this?
+// DB connect
+const connectDB = require("./config/db");
+connectDB();
 
-require("./config/database").connect();
-const cookieParser = require("cookie-parser");
-app.use(cookieParser());
-
-//db se connect krna h
-// const db = require("./config/database");
-// db.connect();
-
-//cloud se connect krna h
-// const cloudinary = require("./config/cloudinary");
-// cloudinary.cloudinaryConnect();
-
+// middleware
 app.use(express.json());
-// const fileupload = require("express-fileupload");
-// app.use(fileupload({
-//     useTempFiles: true,
-//     tempFileDir: path.join(__dirname, "temp/")
-// }));
 
-//activate
+app.get("/", (req, res) => {
+  res.send("Server running 🚀");
+});
+
 app.listen(PORT, () => {
-    console.log(`APP is listening at ${PORT}`);
-})
+  console.log(`APP is listening at ${PORT}`);
+});
---
+++ socialbackend/models/Post.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    content: {
+        type: String,
+        default: "",
+    },
+    image: {
+        type: String,
+        default: "",
+    },
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    ],
+});
+
+export default mongoose.model("Post", postSchema);
+
